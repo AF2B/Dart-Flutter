5edee59ceedc950d487d24d3c706db1ba58e0e8b
André Filipe <dev_andre.filipe@outlook.com>
2022-05-08 03:23:51 -0300

    Create variáveis.dart

---
+++ Dart/fundamentos/variáveis.dart
@@ -0,0 +1,15 @@
+void main() {
+  int x = 2;
+  double pi = 3.1415;
+  String sobrenome = "Fonsêca Borba";
+
+  var y = 5;
+  var nome = "André Filipe";
+  var p = 3.14;
+
+  print(nome + " " + sobrenome);
+  print(pi.runtimeType);
+  print(nome.runtimeType);
+  print(y is String);
+  print(y is int);
+}
